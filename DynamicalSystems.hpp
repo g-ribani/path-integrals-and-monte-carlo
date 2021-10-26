d79eb69b9e715d4dcf1e9074f80d3bd124793e83
gribani <93047578+gribani@users.noreply.github.com>
2021-10-26 23:41:15 +0200

    Add files via upload

--- DynamicalSystems.hpp
+++ DynamicalSystems.hpp
@@ -21,86 +21,101 @@ class DynamicalException : public std::exception {
    std::string _explain;
 };
 
-template<class C, class V> class DynamicalSystem {
+template<class CT, class VT> class DynamicalSystem {
    public:
-   typedef C _CoordType;
-   typedef V _ValueType;
+   typedef CT _CoordType;
+   typedef VT _ValueType;
    typedef std::pair<_CoordType, _ValueType> _DynPair;
-
-   virtual ~DynamicalSystem() = 0;
-   virtual bool IsExactlySolvable() = 0;
-   virtual bool IsDeterministic() = 0;
-   virtual _ValueType operator() (_CoordType) = 0;
-
-   bool HasDiscretisedPath() const { return _discPath.empty(); }
-   void SetDiscretisedPath(const std::vector<_CoordType>& T) {
+   void AddToClassicalPath(_CoordType c) {
       IsDeterministic_Assert();
-      _discPath.clear();
-      for(auto t : T) _discPath.insert_or_assign( t, operator()(t) );
+      _Path.insert_or_assign( c, operator()(c) );
    }
-   void AddPointToDiscretisedPath(double t) {
-      _discPath.insert_or_assign( t, operator()(t) );
+   void AddToPath(_CoordType c, _ValueType v) {
+      _Path.insert_or_assign(c, v);
    }
-   void PrintDiscretisedPath
+   std::map<_CoordType, _ValueType> ClassicalPath
+      (const std::vector<_CoordType>& C) {
+      std::map<_CoordType, _ValueType> P;
+      for(auto c : C) P.insert_or_assign(c, operator()(c) );
+      return P;
+   }
+   std::string ClassName() {
+      return std::string( boost::core::demangle( typeid(*this).name() ) );
+   }
+   void ClearPath() { if( HasPath() ) _Path.clear(); }
+   virtual ~DynamicalSystem() = 0;
+   bool HasPath() const { return !_Path.empty(); }
+   virtual bool IsDeterministic() = 0;
+   virtual bool IsExactlySolvable() = 0;
+   virtual _ValueType operator() (_CoordType) = 0;
+   void PrintPath
       (const std::string& s = "", std::ostream& o = std::cout) const {
-         o << s << _discPath << std::endl;
+         o << s << _Path << std::endl;
    }
-
+   void SetClassicalPath (const std::vector<_CoordType>& C) {
+      IsDeterministic_Assert();
+      ClearPath();
+      for(auto c : C) _Path.insert_or_assign( c, operator()(c) );
+   }
+   virtual void SetBoundaryCondition (_CoordType, _ValueType) = 0;
+   void SetPath(const std::map<_CoordType, _ValueType>& P) {
+      ClearPath();
+      _Path = P;
+   }
    protected:
-   std::map<_CoordType, _ValueType> _discPath;
-
+   std::map<_CoordType, _ValueType> _Path;
    void IsDeterministic_Assert() {
       if(!IsDeterministic()) {
-         std::string s("The motion of the ");
-         s.append( boost::core::demangle( typeid(*this).name() ) )
-            .append(" cannot be determined");
+         std::string s("The motion of the object of type ");
+         s.append( ClassName() )
+          .append(" cannot be determined");
          throw DynamicalException(s);
       }
    }
 };
 
-template<class C, class V> DynamicalSystem<C,V>::~DynamicalSystem() {}
+template<class CT, class VT> DynamicalSystem<CT,VT>::~DynamicalSystem() {}
 
 class EuclideanHarmonicOscillator : public DynamicalSystem<double, double> {
    public:
-   using _CoordType = typename EuclideanHarmonicOscillator::_CoordType;
-   using _ValueType = typename EuclideanHarmonicOscillator::_ValueType;
-   using _DynPair = typename EuclideanHarmonicOscillator::_DynPair;
-
+   double Action() {
+   }
    EuclideanHarmonicOscillator(double omega = 1., double m = 1.)
-      : _freq(omega), _mass(m) {}
+      : _freq(omega), _mass(m) {
+         if(m <= 0.) {
+            std::string s;
+            s.append("Cannot istantiate an object of type ")
+             .append( ClassName() )
+             .append(" with non positive mass");
+            throw DynamicalException(s);
+         }
+         if(omega == 0.)
+            std::clog << "Warning: istantiating an object of type "
+                        << ClassName()
+                        << " with zero frequency\n";
+   }
    ~EuclideanHarmonicOscillator() {};
-
    double Frequency() const { return _freq; }
-   double Mass() const { return _mass; }
-
+   bool IsDeterministic() { return _BCs.size() == 2; }
    bool IsExactlySolvable() { return true; }
-
-   void SetCondition(double tau, double x) {
-      _conds.insert_or_assign(tau,x);
-   }
-
-   bool IsDeterministic() { return _conds.size() == 2; }
-
-   double operator () (double tau) {
+   double Mass() const { return _mass; }
+   _ValueType operator () (_CoordType tau) {
       IsDeterministic_Assert();
-      auto t = GetKeys(_conds);
-      auto x = GetValues(_conds);
-      return ( std::sinh( _freq*(t[1] - tau) )*x[0]
-               + std::sinh( _freq*(tau - t[0]) )*x[1] )
-               / std::sinh( _freq*(t[1] - t[0]) );
+      auto t = GetKeys(_BCs);
+      auto x = GetValues(_BCs);
+      if(_freq != 0.) return ( std::sinh( _freq*(t[1] - tau) )*x[0]
+                              + std::sinh( _freq*(tau - t[0]) )*x[1] )
+                              / std::sinh( _freq*(t[1] - t[0]) );
+      // free particle if _freq == 0
+      else return ( (t[1] - tau)*x[0] + (tau - t[0])*x[1] )/( t[1] - t[0] );
    }
-// implement this if useful
-   double ClassicalPathAction();
-
-   double DiscretizedAction() {
-
+   void SetBoundaryCondition(_CoordType tau, _ValueType x) {
+      _BCs.insert_or_assign(tau,x);
    }
-
    private:
+   std::map<_CoordType, _ValueType> _BCs;
    const double _freq;
    const double _mass;
-   std::map<_CoordType, _ValueType> _conds;
 };
 
 #endif // DYNAMICAL_SYSTEMS_HPP
