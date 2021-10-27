3d52202a6a0c8ef024a41cc8a4d949218afa3a36
gribani <93047578+gribani@users.noreply.github.com>
2021-10-27 10:59:11 +0200

    Add files via upload

--- UtilityFunctions.hpp
+++ UtilityFunctions.hpp
@@ -9,25 +9,64 @@
 
 // returns a linearly spaced vector of n points from in to fin
 std::vector<double>
-   LinearRange(double in, double fin, std::vector<double>::size_type n) {
+   LinearRange(double in, double fin, std::size_t n) {
       std::vector<double> ret;
-      if(n == 1) ret.push_back((in + fin)/2.); // mid point if n == 1
+      if(n == 1) ret.push_back((in + fin)/2.); // midpoint if n == 1
       else if(n > 1) {
-         double step = (fin - in)/(n - 1);
-         for(std::vector<double>::size_type k = 0; k != n; ++k)
-         ret.push_back(in + k*step);
+         const double step = (fin - in)/(n - 1);
+         for(std::size_t k = 0; k != n; ++k) ret.push_back(in + k*step);
       }
       // empty if n == 0
       return ret;
 }
 
 // returns a vector of points from in up to (at most) fin spaced by step
+// Warning: the final point is not guaranteed to be in the range, even if
+// mathematically it would be so, because of machine error.
 std::vector<double> StepRange(double in, double fin, double step) {
-   const std::vector<double>::size_type n = std::round( (fin - in)/step ) + 1;
-   return LinearRange(in,fin,n);
+   std::vector<double> ret;
+   if(in == fin) ret.push_back(in);
+   else if(fin > in) {
+      if(step > 0.) for(double x = in; x <= fin; x += step) ret.push_back(x);
+   }
+   else if (step < 0.)
+         for (double x = in; x >= fin; x += step) ret.push_back(x);
+   return ret;
 }
 
-#ifndef USE_ITERATORS
+#ifdef USE_ITERATORS
+
+// writes std::array on std::ostream
+   template<class T, std::size_t N> std::ostream& operator <<
+      (std::ostream& o, const std::array<T,N>& a) {
+         o << '(';
+         for(auto it = a.begin(); it != a.end()-1; ++it) o << *it << ", ";
+         return o << *(a.end()-1) << ')';
+   }
+
+// writes std::vector on std::ostream
+   template<class T> std::ostream& operator <<
+      (std::ostream& o, const std::vector<T>& v) {
+         o << '(';
+         for(auto it = v.begin(); it != v.end()-1; ++it) o << *it << ", ";
+         return o << *(v.end()-1) << ')';
+   }
+
+// writes std::pair on std::ostream
+   template<class T1, class T2> std::ostream& operator <<
+      (std::ostream& o, const std::pair<T1,T2>& p) {
+         return o << '(' << std::get<0>(p) << ", " << std::get<1>(p) << ")";
+   }
+
+// writes std::map on std::ostream
+   template<class K, class T> std::ostream& operator <<
+      (std::ostream& o, const std::map<K,T>& m) {
+         o << "{ ";
+         for(auto p : m) o << p << ' ';
+         return o << '}';
+   }
+
+#else // do not USE_ITERATORS
 
    template<class T, std::size_t N> std::ostream& operator <<
       (std::ostream& o, const std::array<T,N>& a) {
@@ -60,38 +99,6 @@ std::vector<double> StepRange(double in, double fin, double step) {
       (std::ostream& o, const std::map<K,T>& m) {
          for(auto p : m) o << p << ' ';
          return o;
-   }
-
-#else // USE_ITERATORS
-
-// writes std::array on std::ostream
-   template<class T, std::size_t N> std::ostream& operator <<
-      (std::ostream& o, const std::array<T,N>& a) {
-         o << '(';
-         for(auto it = a.begin(); it != a.end()-1; ++it) o << *it << ", ";
-         return o << *(a.end()-1) << ')';
-   }
-
-// writes std::vector on std::ostream
-   template<class T> std::ostream& operator <<
-      (std::ostream& o, const std::vector<T>& v) {
-         o << '(';
-         for(auto it = v.begin(); it != v.end()-1; ++it) o << *it << ", ";
-         return o << *(v.end()-1) << ')';
-   }
-
-// writes std::pair on std::ostream
-   template<class T1, class T2> std::ostream& operator <<
-      (std::ostream& o, const std::pair<T1,T2>& p) {
-         return o << '(' << std::get<0>(p) << ", " << std::get<1>(p) << ")";
-   }
-
-// writes std::map on std::ostream
-   template<class K, class T> std::ostream& operator <<
-      (std::ostream& o, const std::map<K,T>& m) {
-         o << "{ ";
-         for(auto p : m) o << p << ' ';
-         return o << '}';
    }
 
 #endif // USE_ITERATORS
