665c9f79c2f5a174cc2327cab920ae5c493c826f
Giulio Ribani <93047578+g-ribani@users.noreply.github.com>
2021-11-10 02:31:49 +0100

    Update naive.cpp

--- quantum-mechanics/naive.cpp
+++ quantum-mechanics/naive.cpp
@@ -13,7 +13,7 @@ void PrintProgress (const double &progress,
                     const double &current_estimate,
                     const std::chrono::duration<double>
                         &estimated_time_to_completion,
-                    std::ofstream &out) {
+                    std::ostream &out) {
    std::cout << "["
            << std::setw(3) << int(progress * 100.)
            << "%], estimate = "
@@ -26,7 +26,7 @@ void PrintProgress (const double &progress,
            << ", time to completion = "
            << estimated_time_to_completion.count()
            << " seconds\r" << std::flush;
-   #ifdef PRINT_TO_FILE
+   #ifdef PRINT_LOG
       out << std::setprecision(5) << current_estimate << "\t\t"
           << error_estimate << '\n' << std::flush;
    #endif
@@ -36,7 +36,7 @@ double NaiveMCAmplitude (EuclidHarmonicOscillator1D& osci,
                          const std::size_t n_steps,
                          const std::pair<double, double> range,
                          const double error_goal,
-                         std::ofstream &out) {
+                         std::ostream &out) {
    osci.SetNSteps(n_steps);
    std::vector<std::pair<double, double>> bounds(n_steps-1, range);
    auto F = PathIntegrand<EuclidHarmonicOscillator1D>(osci);
@@ -69,25 +69,30 @@ int main() {
    auto min_max = std::make_pair(xmin, xmax);
    input.clear(), input.close();
 
-   std::ofstream output("naive.out", std::ios::app);
-   #ifdef PRINT_TO_FILE
-      output << "\n\n\t************\t\n\n"
+   // // print to file
+   // std::ofstream output("naive.log", std::ios::app);
+   #ifdef PRINT_LOG
+      std::clog << "\n\n\t************\t\n\n"
             << "(xmin, xmax) = " << min_max << '\n'
             << "nsteps = " << n_steps << '\n'
             << "exact amplitude = " << exact_amp << "\n\n"
             << "estimate\t\terror estimate\n";
    #endif
-   double mc_amp = NaiveMCAmplitude(osci, n_steps, min_max, error_goal, output);
+   double mc_amp =
+   // substitute std::clog with output to print to file
+      NaiveMCAmplitude(osci, n_steps, min_max, error_goal, std::clog);
    std::cout << "(xmin, xmax) = " << min_max << '\n'
              << "nsteps = " << n_steps << '\n'
              << "exact amplitude = " << exact_amp << std::endl
-             << "Naive MC Amplitude: " << mc_amp << " +/- " << mc_amp*error_goal
+             << "Naive MC Amplitude: " << mc_amp
+             << " +/- " << mc_amp*error_goal
              << std::endl;
-   #ifdef PRINT_TO_FILE
-      output << "Naive MC Amplitude: " << mc_amp << " +/- " << mc_amp*error_goal
-             << std::endl;
+   #ifdef PRINT_LOG
+      std::clog << "Naive MC Amplitude: " << mc_amp
+                << " +/- " << mc_amp*error_goal
+                << std::endl;
    #endif
-   output.clear(), output.close();
+   // output.clear(), output.close();
 }
 
 
