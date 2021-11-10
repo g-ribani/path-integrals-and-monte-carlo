17c7d0a869384ce8c0cce4f165114920fff01f03
Giulio Ribani <93047578+g-ribani@users.noreply.github.com>
2021-11-10 01:03:40 +0100

    Add files via upload
    added exact amplitude horizontal line

--- quantum-mechanics/plot.py
+++ quantum-mechanics/plot.py
@@ -7,8 +7,9 @@ results = []
 errors = []
 times = []
 
-first_line = 340       # the first line of the file containing data
-last_line = 420        # the last line
+first_line = 430       # the first line of the file containing data
+last_line = 444        # the last line
+exact_amp = 0.206913
 for n_line in range(first_line, last_line + 1):
     alpha_data = (lnch.getline(file_name, n_line)).split()
     data = [float(d) for d in alpha_data]
@@ -19,7 +20,7 @@ for k in range(first_line, last_line + 1) :
 
 plt.errorbar(times, results, yerr = errors, ecolor = 'lightblue', color='blue', label="naive MC computation")
 plt.xlabel("1 unit = 1 sec of computation (arbitrary offset)")
-plt.hlines(y = 0.206, xmin = first_line, xmax = last_line, color = 'red', label = "exact result")   # exact result
+plt.hlines(y = exact_amp, xmin = first_line, xmax = last_line, color = 'red', label = "exact result")   # exact result
 plt.ylabel("amplitude (hbar = 1)")
 plt.legend(loc="upper left")
 plt.show()
