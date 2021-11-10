26cbd6245a9e690f55366e10bdb3b880877c7cac
Giulio Ribani <93047578+g-ribani@users.noreply.github.com>
2021-11-10 01:04:43 +0100

    Update compile.sh

--- quantum-mechanics/compile.sh
+++ quantum-mechanics/compile.sh
@@ -1,1 +1,1 @@
-g++ -std=c++17 -Wpedantic -Wall -Wextra -DPRINT_ON_FILE -DN_THREADS=2 -O3 -pthread naive.cpp -o naive.exe
+g++ -std=c++17 -Wpedantic -Wall -Wextra -DPRINT_TO_FILE -DN_THREADS=2 -O3 -pthread naive.cpp -o naive.exe
