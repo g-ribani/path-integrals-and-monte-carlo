5122b57fe31391af09b2b40f13ebd1f39cd95439
Giulio Ribani <93047578+g-ribani@users.noreply.github.com>
2021-11-10 01:15:19 +0100

    Update plot.py

--- quantum-mechanics/plot.py
+++ quantum-mechanics/plot.py
@@ -7,9 +7,10 @@ results = []
 errors = []
 times = []
 
-first_line = 430       # the first line of the file containing data
-last_line = 444        # the last line
-exact_amp = 0.206913
+first_line = 180       # lines containing data to extract
+last_line = 263        #
+exact_amp = 0.206913    # todo: automize these two read ops
+simulation_details = "n_steps = 8"  #
 for n_line in range(first_line, last_line + 1):
     alpha_data = (lnch.getline(file_name, n_line)).split()
     data = [float(d) for d in alpha_data]
@@ -18,9 +19,11 @@ for n_line in range(first_line, last_line + 1):
 for k in range(first_line, last_line + 1) :
     times.append(k)
 
-plt.errorbar(times, results, yerr = errors, ecolor = 'lightblue', color='blue', label="naive MC computation")
+plt.errorbar(times, results, yerr = errors, ecolor = 'lightblue', color='blue',
+                label="naive MC, " + simulation_details)
 plt.xlabel("1 unit = 1 sec of computation (arbitrary offset)")
-plt.hlines(y = exact_amp, xmin = first_line, xmax = last_line, color = 'red', label = "exact result")   # exact result
+plt.hlines(y = exact_amp, xmin = first_line, xmax = last_line, color = 'red',
+            label = "exact result")
 plt.ylabel("amplitude (hbar = 1)")
 plt.legend(loc="upper left")
 plt.show()
