44e4dfa865d9f16a8dd31232baa2bc96f7ffa0af
Giulio Ribani <93047578+g-ribani@users.noreply.github.com>
2021-11-11 13:42:31 +0100

    Update plot.py

--- dynamical-systems/plot.py
+++ dynamical-systems/plot.py
@@ -2,38 +2,66 @@ from matplotlib import pyplot as plt
 import linecache as lnch
 import numpy as np
 
-file_name = 'main.log'
-
 results = []
 errors = []
 evals = []
 
-first_line = 424       # lines containing data to extract
-last_line = 628        #
+crude_file_name = 'crude.log'
+crude_first_line = 9       # lines containing data to extract
+crude_last_line = 129       # lines containing data to extract
+
+vegas_file_name = 'vegas.log'
+vegas_first_line = 37        #
+vegas_last_line = 157        #
+
 exact_amp = 2.069131e-01    # todo: automize these the following read ops
-n_steps = 12                 #
-n_evals = 2e6               #
-simulation_details = "n_steps = " + str(n_steps)  #
-for n_line in range(first_line, last_line + 1) :
-    if ((n_line - first_line) % 3 == 0):
-        alpha_data = (lnch.getline(file_name, n_line)).split()
+n_evals = 1e7               #
+
+max_n = max(crude_last_line - crude_first_line,
+            vegas_last_line - vegas_first_line)
+plt.hlines(y = exact_amp, xmin = 0, xmax = max_n, color = 'red',
+            label = "exact =" + str(round(exact_amp,4)))
+
+# plotting crude data
+for n_line in range(crude_first_line, crude_last_line + 1) :
+    if ((n_line - crude_first_line) % 2 == 0):
+        alpha_data = (lnch.getline(crude_file_name, n_line)).split()
         results.append(float(alpha_data[4]))
         errors.append(float(alpha_data[6]))
-        evals.append(n_line)
+        evals.append(n_line - crude_first_line)
 
 average = np.mean(results)
 error = np.std(results)/np.sqrt(len(results))
-print("exact =", exact_amp)
-print("estimate =", average, " +/- ", error)
 
-plt.errorbar(evals, results, yerr = errors, ecolor = 'lightblue', color='blue',
-                label= ("crude MC, " + simulation_details))
-plt.xlabel("1 unit = " + str(n_evals) +
-            " function evaluations (arbitrary offset()")
-plt.hlines(y = exact_amp, xmin = first_line, xmax = last_line, color = 'red',
-            label = "exact result")
-plt.hlines(y = average, xmin = first_line, xmax = last_line,
-            color = 'green', label = "average result")
+plt.errorbar(evals, results, yerr = errors, ecolor = 'lightblue',
+                color='blue', label= "crude MC")
+plt.hlines( y = average, xmin = 0, xmax = max_n, color = 'green',
+            label = "crude average =" + str(round(average,4))
+                                + " +/- " + str(round(error,4)) )
+
+# plotting vegas data
+results = []
+errors = []
+evals = []
+
+for n_line in range(vegas_first_line, vegas_last_line + 1) :
+    if ((n_line - vegas_first_line) % 2 == 0):
+        alpha_data = (lnch.getline(vegas_file_name, n_line)).split()
+        results.append(float(alpha_data[4]))
+        errors.append(float(alpha_data[6]))
+        evals.append(n_line - vegas_first_line)
+
+average = np.mean(results)
+error = np.std(results)/np.sqrt(len(results))
+
+plt.errorbar(evals, results, yerr = errors, ecolor = 'moccasin',
+                color='orange', label= "Vegas MC")
+plt.hlines( y = average, xmin = 0, xmax = 0, color = 'orange',
+            label = "Vegas average =" + str(round(average,6))
+                                + " +/- " + str(round(error,6)) )
+
+plt.xlabel("1 unit = " + str(int(n_evals)) +
+            " function evaluations (arbitrary offset)")
 plt.ylabel("amplitude (hbar = 1)")
 plt.legend(loc="upper left")
 plt.show()
