1fb725511714e432515b673c9ddda132f513f4a8
Giulio Ribani <93047578+g-ribani@users.noreply.github.com>
2021-11-11 12:42:04 +0100

    Update plot.py

--- quantum-mechanics/plot.py
+++ quantum-mechanics/plot.py
@@ -2,33 +2,31 @@ from matplotlib import pyplot as plt
 import linecache as lnch
 import numpy as np
 
-file_name = 'naive-quater.log'
+file_name = 'naive-5.log'
 
 results = []
 errors = []
 times = []
 
-first_line = 65       # lines containing data to extract
-last_line = 326       #
-exact_amp = 5.662614e-02    # todo: automize these two read ops
-n_steps = 10
-simulation_details = "n_steps = " + str(n_steps)  #
+first_line = 10       # lines containing data to extract
+last_line = 861       #
+exact_amp = 0.206913    # todo: automize this read ops
 for n_line in range(first_line, last_line + 1):
     alpha_data = (lnch.getline(file_name, n_line)).split()
     data = [float(d) for d in alpha_data]
     results.append(data[0])
     errors.append(data[0]*data[1])
     times.append(n_line)
-mean = np.mean(results)
-print("mean =", mean)
+
+best = results[len(results)-1]
 
 plt.errorbar(times, results, yerr = errors, ecolor = 'lightblue', color='blue',
-                label= "naive MC, " + simulation_details)
+                label= "naive MC")
 plt.xlabel("1 unit = 1 sec of computation (arbitrary offset)")
 plt.hlines(y = exact_amp, xmin = first_line, xmax = last_line, color = 'red',
-            label = "exact result")
-plt.hlines(y = mean, xmin = first_line, xmax = last_line, color = 'green',
-            label = "mean result")
+            label = "exact =" + str(exact_amp))
+plt.hlines(y = best, xmin = first_line, xmax = last_line, color = 'green',
+            label = "best estimate = " + str(best))
 plt.ylabel("amplitude (hbar = 1)")
 plt.legend(loc = "upper left")
 plt.show()
