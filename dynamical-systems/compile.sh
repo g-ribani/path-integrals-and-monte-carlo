6b13ddcd33f449559cc0479ec8798da62b805fbc
Giulio Ribani <93047578+g-ribani@users.noreply.github.com>
2021-11-11 10:28:54 +0100

    Update compile.sh

--- dynamical-systems/compile.sh
+++ dynamical-systems/compile.sh
@@ -1,2 +1,2 @@
-g++ -std=c++17 -Wpedantic -Wall -Wextra -c main.cpp -DBE_VEGAS -DBE_CRUDE
-g++ main.o -lgsl -lgslcblas -lm -o main.exe
+g++ -std=c++17 -Wpedantic -Wall -Wextra -c main.cpp -DBE_CRUDE -DBE_VEGAS
+g++ main.o -lgsl -lgslcblas -lm -o main
